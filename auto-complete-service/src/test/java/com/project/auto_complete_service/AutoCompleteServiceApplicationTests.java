dc3bdb60417c7d21121e5985beeec408916566fa
ak9623019 <ak9623019@gmail.com>
2026-04-18 00:39:13 +0530

    Add Application Setup

---
+++ auto-complete-service/src/test/java/com/project/auto_complete_service/AutoCompleteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.auto_complete_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCompleteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
